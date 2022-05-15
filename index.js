847af15a85cf8b9c790bd05548b57449cff98e19
sukanyagovindan <gsukanya@gmail.com>
2022-05-14 19:47:32 -0700

    Submit Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+// Write your solution in this file
+const employee = {name : "", streetAddress : ""};
+
+function updateEmployeeWithKeyAndValue(employee,somekey, somevalue) {
+    const employeecopy = {...employee}
+    employeecopy[somekey] = somevalue;
+    return employeecopy;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,somekey, somevalue) {
+    employee[somekey] = somevalue;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee,somekey) {
+    const employeecopy = {...employee}
+    delete employeecopy[somekey];
+    return employeecopy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,somekey) {
+    delete employee[somekey];
+    return employee;
+}
